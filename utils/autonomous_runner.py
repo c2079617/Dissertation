29db9b47f9aaf48257248a9e5166de3ba8176015
Jack Kong <jack@Jacks-MacBook-Pro.local>
2025-05-08 22:29:47 +0100

    fixed auto_runner.py

--- utils/autonomous_runner.py
+++ utils/autonomous_runner.py
@@ -1,7 +1,7 @@
 import time
 import os
 import sys
-from imap_tools import MailMessageFlags
+from imap_tools import MailMessageFlags, MailBox, AND
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from main import main 
 
@@ -9,17 +9,23 @@ def has_new_files(receipt_folder="receipts"):
     # Check if any .jpg files exist that haven't been processed
     return any(f.endswith('.jpg') for f in os.listdir(receipt_folder))
 
+def has_unseen_emails():
+    from_email = os.getenv("EMAIL_USER")
+    password = os.getenv("EMAIL_PASS")
+    with MailBox('imap.gmail.com').login(from_email, password) as mailbox:
+        unseen_messages = list(mailbox.fetch(criteria=AND(seen=False), limit=1))
+        return len(unseen_messages) > 0
+
 def run_autonomously(poll_interval=60):  # check every 60 seconds
     print("[*] Autonomous runner started...")
 
     while True:
-        print("[*] Checking for new receipts...")
-
-        if has_new_files():
-            print("[*] New receipt found! Running main...")
+        print("[*] Checking for new unread emails...")
+        if has_unseen_emails():
+            print("[*] New unseen email(s) found! Running main...")
             main()
         else:
-            print("[*] No new receipts. Sleeping...\n")
+            print("[*] No new unread emails. Sleeping...\n")
 
         time.sleep(poll_interval)
 
